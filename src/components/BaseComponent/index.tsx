dbbe57608adb36f9bbfba2b8bdc1a69b9f1c7342
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-09-10 21:31:24 +0300

    Test

--- src/components/BaseComponent/index.tsx
+++ src/components/BaseComponent/index.tsx
@@ -7,6 +7,7 @@ import styles from './styles';
 const pages = [
     { title: 'Home', arrow: false },
     { title: 'Home2', arrow: false },
+    { title: 'Home3', arrow: false },
 ];
 
 type ComponentProps = {
